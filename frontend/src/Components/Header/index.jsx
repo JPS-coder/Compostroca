36989246308a1d3e76f998ee4c6b99792d9cffae
Nivia Alves <niviafalves@hotmail.com>
2025-01-23 23:04:37 -0300

    Adicionando menu hamburger

--- frontend/src/Components/Header/index.jsx
+++ frontend/src/Components/Header/index.jsx
@@ -11,7 +11,25 @@ function Header() {
     return (
         <header className={styles.header}>
             <img className={styles.logo} src={logo} alt='Logotipo compostroca'/>
-            
+            <nav
+            className={`${styles.menuSandwich} ${ showMenu ? styles.show : ''}`}
+            onClick={toggleMenu}
+            >
+                <Link to="/">Home</Link>
+                <Link to="/RegisterLogin">Editar Dados</Link>
+                <Link to="/OrganicDelivery">Entrega de Resíduos</Link>
+                <Link to="/StationData">Dados da Estação</Link>
+                <Link to="/Info">Informaçoes</Link>   
+            </nav>
+      
+            <div
+             className={styles.menuButton}
+             onClick={toggleMenu}>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+                <span className={styles.linha}></span>
+            </div>
+
 
         </header>
     )
