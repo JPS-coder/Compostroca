4428bf2453d58a2c8128d381be7febc625c89019
Nivia Alves <niviafalves@hotmail.com>
2025-01-28 00:00:12 -0300

    Criado a conexao com MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
-import express from express
-import cors from cors
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
+import wasteRoutes from './routes/waste.js';
+import connectDB from './db.js';
+
+
+dotenv.config()
 
 //Inicialização do servidor criando uma instância para configurar as rotas e os middlewares.
 const app = express();
+connectDB();
 
 app.use(express.json()); // middleware p/ processar e disponibilizar requisições tipo json
 app.use(cors()); // habilita o cors, p/ aceitar requisições de diferentes origens 
 
+app.use('/auth', authRoutes);
+app.use('/waste', wasteRoutes);
 // definição de rota, objeto de requisição e de resposta
 
 app.get('/', (req, res) => {
