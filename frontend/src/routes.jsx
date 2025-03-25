09210755912127f3f92e98011453bfa4cfe7684d
nivia_alves <niviafalves@hotmail.com>
2025-03-25 15:26:30 -0300

    Adicionado rotas das novas páginas

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -8,7 +8,11 @@ import Home from "./Pages/Home";
 import OrganiciDelivery from "./Pages/OrganicDelivery";
 import RegisterNewUser from "./Pages/LoginRegister/registerNewUser";
 import Edit from "./Pages/LoginRegister/edit"
-
+import Composting from "./Pages/Composting";
+import GardenCare from "./Pages/GardenCare"
+import Cooking from "./Pages/Cooking";
+import CategoryPage from "./Pages/Cooking/categoryPage"
+import PostPage from "./Pages/Cooking/postPage"
 
 function AppRoutes() {
     return(
@@ -23,6 +27,11 @@ function AppRoutes() {
                     <Route path="/stationData" element={ <StationData/> } />
                     <Route path="/menu" element={ <Menu/> } />
                     <Route path="/organicDelivery" element={ <OrganiciDelivery/> } />
+                    <Route path="/composting" element={ <Composting/> } />
+                    <Route path="/gardenCare" element={ <GardenCare/> } />
+                    <Route path="/cooking" element={ <Cooking/> } />
+                    <Route path="/post/:id" element={<PostPage />} />
+                    <Route path="/category/:categoryName" element={<CategoryPage />} />
                     
                 </Route>
             </Routes>
