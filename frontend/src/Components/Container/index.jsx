4b8044d3f0b3d0add8738081db6ac49e1bca3454
nivia_alves <niviafalves@hotmail.com>
2025-04-17 21:53:40 -0300

    Ajustando imagem de fundo

--- frontend/src/Components/Container/index.jsx
+++ frontend/src/Components/Container/index.jsx
@@ -1,11 +1,12 @@
 import styles from './Container.module.css'
+import background from '../../assets/images/background.svg'
 
 
 function Container ({ children }) {
     return (
         
         <section className={styles.container}>
-           
+                    
             { children }
         </section>
     )
