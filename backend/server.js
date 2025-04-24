70ca6ea91004920d15343548e27ed719f6947d5a
Nivia Alves <niviafalves@hotmail.com>
2025-04-24 18:32:45 -0300

    Configurado criaçao de tabelas

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import wasteRoutes from './routes/waste.js';
 import stationdataRoutes from './routes/station.js'
+import pool from './database.js'
 
 dotenv.config()
 
@@ -20,4 +21,25 @@ app.get('/', (req, res) => {
     res.send ('API funcionando!')
 });
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+const createTables = async () => {
+    try {
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS station_data (
+                id SERIAL PRIMARY KEY,
+                name_station VARCHAR(255),
+                vol_station NUMERIC,
+                vol_person NUMERIC,
+                vol_total NUMERIC,
+                co2_avoided NUMERIC
+            );
+        `);
+        console.log("Tabela 'station_data' verificada/criada com sucesso.");
+    } catch (err) {
+        console.error("Erro ao criar tabela:", err);
+    }
+};
+
+app.listen(3000, async () => {
+    await createTables();
+    console.log('Servidor rodando na porta 3000');
+})
